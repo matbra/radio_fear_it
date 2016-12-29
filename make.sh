0f7ffb7da98339884ffa679aa50007362989de69
Matthias Brandt <der.matthias.brandt@gmail.com>
2016-12-29 14:47:21 +0100

    Work on the file to install dependencies.

--- make.sh
+++ make.sh
@@ -3,38 +3,42 @@
 git submodule init
 git submodule update
 
+# determine whether we're on openshift
+DIR_OUTPUT=${OPENSHIFT_DATA_DIR:?"./src/build"}
+DIR_TEMP=${OPENSHIFT_DATA_DIR:?"./tmp"}
+
 # install autoconf
-cd $OPENSHIFT_TMP_DIR
+cd $DIR_TEMP
 wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 tar xzf autoconf-2.69.tar.gz
 cd autoconf-2.69
-./configure --prefix=$OPENSHIFT_DATA_DIR
+./configure --prefix=$DIR_OUTPUT
 make
-make install DESTDIR=$OPENSHIFT_DATA_DIR
+make install DESTDIR=$DIR_OUTPUT
 
-export PATH=$OPENSHIFT_DATA_DIR/usr/local/bin:$PATH
-export LD_LIBRARY_PATH=$OPENSHIFT_DATA_DIR/var/lib
+export PATH=$DIR_OUTPUT/usr/local/bin:$PATH
+export LD_LIBRARY_PATH=$DIR_OUTPUT/var/lib
 
 # install automake
-cd $OPENSHIFT_TMP_DIR
+cd $DIR_TEMP
 wget http://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz
 tar xzf automake-1.15.tar.gz
 cd automake-1.15
-./configure --prefix=$OPENSHIFT_DATA_DIR
+./configure --prefix=$DIR_OUTPUT
 make
-make install DESTDIR=$OPENSHIFT_DATA_DIR
+make install DESTDIR=$DIR_OUTPUT
 
 # libtool
-cd $OPENSHIFT_TMP_DIR
+cd $DIR_TEMP
 wget http://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.gz
 tar xzf libtool-2.4.6.tar.gz
 cd libtool-2.4.6
-./configure --prefix=$OPENSHIFT_DATA_DIR
+./configure --prefix=$DIR_OUTPUT
 make
-make install DESTDIR=$OPENSHIFT_DATA_DIR
+make install DESTDIR=$DIR_OUTPUT
 
 alias aclocal-1.15='aclocal'
 
-ln -s /usr/bin/aclocal ${OPENSHIFT_DATA_DIR}bin/aclocal-1.15
+#ln -s /usr/bin/aclocal ${$DIR_OUTPUT}bin/aclocal-1.15
 
 # install bison
