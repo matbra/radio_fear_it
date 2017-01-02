8fb8db9ad94f5b073df43784477d2cbbacb1b431
Matthias Brandt <der.matthias.brandt@gmail.com>
2017-01-02 16:25:35 +0100

    Add speech recognition by pocketsphinx.

--- src/python/main.py
+++ src/python/main.py
@@ -5,11 +5,16 @@ elif sys.version_info.major == 3:
     from urllib.request import urlopen
 else:
     raise("python version maybe not supported.")
+from os.path import join, dirname
 import wave
 import struct
-from pymp3decoder import Decoder
+from pymp3decoder import Decoder as Decoder_mp3
+
+from pocketsphinx.pocketsphinx import *
+from sphinxbase.sphinxbase import  *
 
 CHUNK_SIZE = 4096
+MODELDIR = join(dirname(__file__), "../sphinx_models/de_DE")
 
 def find_frame_start(buffer):
     buf_conv = struct.unpack_from("B" * len(buffer), buffer)
@@ -24,7 +29,7 @@ def find_frame_start(buffer):
 def streamurl(url):
 
     # initialize mp3 decoder
-    dec = Decoder(20*CHUNK_SIZE)
+    dec_mp3 = Decoder_mp3(20*CHUNK_SIZE)
 
     u = urlopen(url)
 
@@ -33,6 +38,14 @@ def streamurl(url):
     ww.setnchannels(1)
     ww.setframerate(44100)
     ww.setsampwidth(2)
+
+    # prepare pocketsphinx
+    ps_config = Decoder.default_config()
+    ps_config.set_string('-hmm', join(MODELDIR, 'de_DE'))
+    ps_config.set_string('-lm', join(MODELDIR, 'voxforge.lm.DMP'))
+    ps_config.set_string('-dict', join(MODELDIR, 'voxforge.dic'))
+    ps_config.set_string('-logfn', '/dev/null')
+    dec_speech = Decoder(ps_config)
 
     # find the start index
     idx_start = None
@@ -46,7 +59,7 @@ def streamurl(url):
         try:
             last = bytearray([])
             chunk = n[idx_start:]
-            decoded, last = dec.decode(chunk, last)
+            decoded, last = dec_mp3.decode(chunk, last)
             print("started successfully.")
             break
         except:
@@ -57,13 +70,36 @@ def streamurl(url):
     chunk = n[idx_start:]
 
     last = bytearray([])
+    dec_speech.start_utt()
+    in_speech_bf = True
     while True:
-        decoded, last = dec.decode(chunk, last)
+        decoded, last = dec_mp3.decode(chunk, last)
         chunk = u.read(CHUNK_SIZE)
 
         # extract the left channel (wave interleaved format)
         left = b"".join([decoded[_:_+2] for _ in range(0, len(decoded), 4)])
         ww.writeframes(left)
+
+        dec_speech.process_raw(left, False, False)
+        try:
+            if dec_speech.hyp().hypstr != "":
+                print("partial decoding result: ", dec_speech.hyp().hypstr)
+        except AttributeError:
+            pass
+
+        if dec_speech.get_in_speech():
+            pass
+
+        if dec_speech.get_in_speech() != in_speech_bf:
+            in_speech_bf = dec_speech.get_in_speech()
+            if not in_speech_bf:
+                dec_speech.end_utt()
+                try:
+                    if dec_speech.hyp().hypstr != '':
+                        print('Stream decoding result:', dec_speech.hyp().hypstr)
+                except AttributeError:
+                    pass
+                dec_speech.start_utt()
 
         if chunk == "":
             print("stream ended.")
